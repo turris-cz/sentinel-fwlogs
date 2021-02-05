0f4ba09ba922919e9181f14c325b123b2c6c71b8
Karel Kočí <karel.koci@nic.cz>
2021-02-05 18:31:55 +0100

    Fixes for eb7c4caeb2a471ddaf6239d95c899c10b2ebeec8
    This removes typo in gitignore, unsecure git (that was just used to
    overcome temporally bug in Debian and invalid names of release archives.

--- .gitlab-ci/release.sh
+++ .gitlab-ci/release.sh
@@ -4,8 +4,8 @@ set -eu
 VERSION="$(echo "${CI_COMMIT_TAG}" | sed -nE 's/v([0-9]+)\.([0-9]+)\.([0-9]+).*/\1.\2.\3/p')"
 
 declare -a args
-for dist in base64c-*.tar.gz base64c-*.tar.xz base64c-*.zip; do
-	URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/base64c/${VERSION}/${dist}"
+for dist in sentinel-fwlogs-*.tar.gz sentinel-fwlogs-*.tar.xz sentinel-fwlogs-*.zip; do
+	URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/sentinel-fwlogs/${VERSION}/${dist}"
 	curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file "${dist}" "${URL}"
 	args+=("--assets-link" "{\"name\":\"${dist}\",\"url\":\"${URL}\"}")
 done
