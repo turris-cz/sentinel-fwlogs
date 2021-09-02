f99f9e5474d0787d81a99caf937a0ec21378b070
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-09-02 15:12:22 +0200

    fwrecorder: improve includes

--- recorder/fwrecorder.c
+++ recorder/fwrecorder.c
@@ -1,13 +1,7 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <argp.h>
-#include <time.h>
-#include <arpa/inet.h>
-#include <libnetfilter_log/libnetfilter_log.h>
 #define DEFLOG log_fwrecorder // logger used in default in logc macros
 #include <logc.h>
 #include <logc_util.h>
