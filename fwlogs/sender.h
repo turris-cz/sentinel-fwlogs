f4237f628cb625713e8b93939e1eae0367485f60
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-09-02 14:26:47 +0200

    sender: improvements
    - sort includes
    - add attribute nonnull to function declarations

--- fwlogs/sender.h
+++ fwlogs/sender.h
@@ -1,5 +1,6 @@
 #ifndef _SENTINEL_FWLOGS_SENDER_H_
 #define _SENTINEL_FWLOGS_SENDER_H_
+#include <stdbool.h>
 #include "parser.h"
 
 struct sender;
@@ -7,14 +8,15 @@ typedef struct sender* sender_t;
 
 // Allocate and initialize new sender
 // Returns sender_t instance or NULL in case of of invalid socket
-sender_t sender_new(const char *socket, const char *topic) __attribute__((malloc));
+sender_t sender_new(const char *socket, const char *topic)
+	__attribute__((malloc,nonnull));
 
 // Send packet_data with given sender
 // Returns true on success and false otherwise
-bool sender_send(sender_t, struct packet_data*);
+bool sender_send(sender_t, struct packet_data*) __attribute__((nonnull));
 
 // Free sender instance resources
-void sender_destroy(sender_t);
+void sender_destroy(sender_t) __attribute__((nonnull));
 
 #endif
 
